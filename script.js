12f41b5887af0a8f823b8b6022d2e0cccafb048f
asad-khilji <100327772+asad-khilji@users.noreply.github.com>
2026-06-07 13:50:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Asad",
+    age: 33,
+    color: "white"
+};
+
+document.getElementById("name").innerHTML += person.name 
+document.getElementById("age").innerHTML += person.age 
+document.getElementById("color").innerHTML += person.color 
+
+
